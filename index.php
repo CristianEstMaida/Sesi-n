5680610d34c27c0443e8df8a2b6e2157d48c25e9
CristianEstMaida <55913935+CristianEstMaida@users.noreply.github.com>
2023-03-08 20:20:25 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,59 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title></title>
-	<link rel="stylesheet" href="">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
 	<form action="procesa_login.php" method="POST">
-		<table>
-			<tr>
-				<td>Usuario</td>
-				<td><input type="text" name="usuario"></td>
-			</tr>			
-			<tr>
-				<td>Contraseña</td>
-				<td><input type="password" name="clave"></td>
-			</tr>
-			<tr>
-				<td></td>
-				<td><input type="submit" value="Enviar"></td>
-			</tr>
-		</table>
+	    
+	    <section class="vh-100 gradient-custom">
+          <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+              <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                  <div class="card-body p-5 text-center">
+        
+                    <div class="mb-md-5 mt-md-4 pb-5">
+        
+                      <h2 class="fw-bold mb-2 text-uppercase">Iniciar sesión</h2>
+                      <p class="text-white-50 mb-5">Por favor ingrese su usuario y contraseña</p>
+        
+                      <div class="form-outline form-white mb-4">
+                        <input type="text" id="typeEmailX" name="usuario" class="form-control form-control-lg" />
+                        <label class="form-label" for="typeEmailX">Usuario</label>
+                      </div>
+        
+                      <div class="form-outline form-white mb-4">
+                        <input type="password" id="typePasswordX" name="clave" class="form-control form-control-lg" />
+                        <label class="form-label" for="typePasswordX">Contraseña</label>
+                      </div>
+        
+                      <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="registro.php">¿Olvidé la contraseña?</a></p>
+        
+                      <input class="btn btn-outline-light btn-lg px-5" type="submit" value="Iniciar sesión">
+        
+                    </div>
+        
+                    <div>
+                      <p class="mb-0">¿No tenés una cuenta? <a href="registro.php" class="text-white-50 fw-bold">Registrarse</a>
+                      </p>
+                    </div>
+        
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        
 	</form>
-	<p>Si no estas registrado , hace click <a href="registro.php">aqui</a></p>
-	
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
